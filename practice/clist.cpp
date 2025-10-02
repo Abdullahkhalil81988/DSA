5bfdeeb7aece52cfc41f5a90f260fa89653201c6
Abdullahkhalil81988 <akhalil.bsai24seecs@seecs.edu.pk>
2025-10-02 21:02:21 +0500

    fia

--- practice/clist.cpp
+++ practice/clist.cpp
@@ -2,44 +2,37 @@
 using namespace std;
 
 
-int main() {
-   class Node{
-    public:
+class Node {
+public:
     int data;
     Node* next;
-    Node(int val){
+    Node(int val) {
         data = val;
+        next = nullptr;
     }
+};
 
-   };
-   class circularlist{
-    public:
+class circularlist {
+public:
     Node* head;
     Node* tail;
     circularlist() {
-        head = tail = NULL;
+        head = tail = nullptr;
     }
-    void inserthead(int val){
+    void inserthead(int val) {
         Node* newNode = new Node(val);
-        if (head == NULL)
-        {
+        if (head == nullptr) {
             head = tail = newNode;
             newNode->next = head;
-            head = newNode;
-            tail->next =head;
-           
-        }
-        else{
-            Node* newNode= new  Node(val);
+        } else {
             newNode->next = head;
             head = newNode;
-            tail->next = newNode;
-
+            tail->next = head;
         }
     }
 
     void printList() {
-        if (head == NULL) {
+        if (head == nullptr) {
             cout << "List is empty." << endl;
             return;
         }
@@ -50,6 +43,40 @@ int main() {
         } while (temp != head);
         cout << endl;
     }
-   };
+
+    void inserttail(int val) {
+        Node* newNode = new Node(val);
+        if (head == nullptr) {
+            head = tail = newNode;
+            newNode->next = head;
+        } else {
+            tail->next = newNode;
+            newNode->next = head;
+            tail = newNode;
+        }
+        
+    }
+void deleteAtHead () {
+    if (head == nullptr) return;
+    else if (head == tail) { // single node
+        delete head;
+        head = tail = nullptr;
+    } else {
+        // 2 or more nodes
+        Node* temp = tail;
+        head = head->next;
+        tail->next = head;
+        temp->next = nullptr;
+        delete temp;
+    }
+}
+};
+
+int main() {
+    circularlist cl;
+    cl.inserthead(1);
+    cl.inserthead(2);
+    cl.inserttail(3);
+    cl.printList();
     return 0;
 }
